9babcd08a88bd1c451bb8f00e6425b2315ec0830
Shujaat Hussain <sujathussain426@gmail.com>
2025-10-11 10:01:47 +0530

    refactor: update configuration to use table ID instead of collection ID and remove project name

--- src/appwrite/config.post.js
+++ src/appwrite/config.post.js
@@ -1,1 +1,50 @@
-import { Client,Databases,ID } from "appwrite";
+import { Client, ID, TablesDB } from "appwrite";
+
+import conf from "../conf/conf.js";
+
+export class ConfigService {
+  client = new Client();
+  databases;
+
+  constructor() {
+    this.client.setEndpoint(conf.appwriteurl).setProject(conf.projectid);
+    this.databases = new TablesDB(this.client);
+  }
+
+  async createRow(title,description,isComplete) {
+    try {
+      const row = await this.databases.createRow({
+        databaseId:conf.databaseid,
+        tableId: conf.tableid,
+        rowID: ID.unique(),
+        title,
+        description,
+        isComplete
+      });
+      if(row){
+        return row
+      } else {
+        console.log("Row creation failed");
+      }
+    } catch (error) {
+      console.error("Error creating row: in createRow()::", error);
+      throw error;
+    }
+  }
+    async getRows() {
+    try {
+      const rows = await this.databases.listRows({
+        databaseId: conf.databaseid,
+        tableId: conf.tableid
+      });
+      if (rows) {
+        return rows;
+      } else {
+        console.log("No rows found");
+      }
+    } catch (error) {
+      console.error("Error fetching rows: in getRows()::", error);
+      throw error;
+    }
+  }
+}
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -2,9 +2,8 @@ const conf = {
     appwriteurl: String(import.meta.env.VITE_APPWRITE_ENDPOINT),
     projectid: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
     databaseid: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
-    collectionid: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    tableid: String(import.meta.env.VITE_APPWRITE_TABLE_ID),
     bucketid: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    projectname: String(import.meta.env.VITE_APPWRITE_PROJECT_NAME)
 }
 
 export default conf;
