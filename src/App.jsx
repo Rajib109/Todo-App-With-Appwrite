95f09ee971ed6cecef4ab05b1b1fd0b35038c424
Shujaat Hussain <sujathussain426@gmail.com>
2025-10-30 15:56:46 +0530

    feat: enhance UI by improving layout and styling in App and AddTodo components

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Todos from "./components/Todos";
 
 function App() {
   return (
-    <div className="bg-gray-700 h-screen">
-      <h1>Manage Your Todos</h1>
+    <div className="bg-gray-700 h-screen w-full p-8">
+      <h1 className="text-2xl font-bold mb-4">Manage Your Todos</h1>
       <AddTodo />
       <Todos />
     </div>
