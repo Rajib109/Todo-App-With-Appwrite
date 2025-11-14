b6becea7c7d9cfe3ec1686a2cbcc5930c38682b9
Shujaat Hussain <sujathussain426@gmail.com>
2025-11-14 17:17:56 +0530

    feat: update Home component to include Header, Outlet, and Footer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
+import {Header,Footer} from './index'
 
 const Home = () => {
     return (
         <div>
-            
+            <Header/>
+            <Outlet/>
+            <Footer/>
         </div>
     );
 }
