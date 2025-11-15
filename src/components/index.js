dc2b6d8a54bedf428f0a847b8c3985f32860f914
Shujaat Hussain <sujathussain426@gmail.com>
2025-11-15 20:34:10 +0530

    feat: update index file to include Home, Login, Header, Footer, Protected, and Container components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,9 @@
 import AddTodo from "./AddTodo";
-import TodoList from "./TodoList";
-export { AddTodo, TodoList }
+import Home from "./Home";
+import Login from './Login';
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Protected from './Protected'
+import Container from './container/Container'
+
+export { AddTodo, Home, Login, Header, Footer, Protected, Container }
